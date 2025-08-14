b841cced090f67be25a36cc74f0aea2fd867ebe4
bayu-Apps <bayurara555@gmail.com>
2025-08-14 13:26:29 +0700

    connected to postgres DB

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/bayuTri-Code/Auth-Services/database"
+	"github.com/bayuTri-Code/Auth-Services/internal/config"
+	"github.com/gin-gonic/gin"
+)
+
+//testing
+func homeTesting(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
+}
 
 func main(){
-	fmt.Println("TISTING PROJECT")
+	config.ConfigDb()
+	database.PostgresConn()
+
+	//testing
+	router := gin.Default()
+	router.GET("/", homeTesting)
+	if err := router.Run(); err != nil {
+		return
+	}
+	
 }
